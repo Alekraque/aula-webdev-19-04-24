6bbad1e2c23df1a480b9b9d20cfe8f5f9a112958
Alex Ribeiro Maia <161780361+Alekraque@users.noreply.github.com>
2024-04-20 01:19:55 -0300

    Update index.js com codigo dos cargos

--- js/index.js
+++ js/index.js
@@ -21,7 +21,27 @@ function calcularMulta(){
     resultadoMulta.innerText = `O valor da sua multa é de ${multa} reais`
 }
 
-const cargo = document.getElementById('cargo').value
+function calc(){
+const cargo = document.getElementById('valorCargo').value;
 let salario = 3_000
-
-switch 
+switch (cargo){
+    case "junior":
+        salario *=1
+    break;
+    case "pleno":
+        salario *=2
+    case "senior":
+        salario *=3
+    break;
+    case "techLead":
+        salario *=4
+    break;
+    case "diretor":
+        salario *=10
+    break;
+    default:
+        salario = 0
+    resultado.innerText='este cargo não existe'
+}
+    resultado.innerText=`O salario de ${cargo} é de R$ ${salario}`;
+}
